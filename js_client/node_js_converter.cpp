6c408f448c6e188488172f218bd43d0d5279a29c
Hojin Ghim <hojin@amazon.com>
2020-03-13 18:44:01 +0000

    Add node_js_converter module depending on node_addon_api

---
+++ js_client/node_js_converter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hojin Ghim on 13/03/2020.
+//
+
+#include "node_js_converter.hpp"
---
+++ js_client/node_js_converter.hpp
@@ -0,0 +1,34 @@
+//
+// Created by Hojin Ghim on 13/03/2020.
+//
+
+#ifndef HAWKTRACER_NODE_JS_CONVERTER_HPP
+#define HAWKTRACER_NODE_JS_CONVERTER_HPP
+
+#include "converter.hpp"
+#include <napi.h>
+
+namespace HawkTracer
+{
+namespace client {
+
+class NodeJsConverter : public Converter {};
+
+}
+}
+
+static Napi::Object Init(Napi::Env env, Napi::Object exports) {
+  // Create a new instance
+  Napi::Object obj = Napi::Object::New(env);
+
+  // Assign values to properties
+  obj.Set("hello", "world");
+  obj.Set(uint32_t(42), "The Answer to Life, the Universe, and Everything");
+  obj.Set("Douglas Adams", true);
+  exports.Set(Napi::String::New(env, "hello"), obj);
+  return exports;
+}
+
+NODE_API_MODULE(hello, Init)
+
+#endif // HAWKTRACER_NODE_JS_CONVERTER_HPP
