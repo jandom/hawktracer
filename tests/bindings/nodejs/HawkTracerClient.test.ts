49224b2d8b11d9499a2ac9633469752eabbeb60b
Hojin Ghim <hojin@amazon.com>
2020-03-19 20:12:56 +0000

    Test 2. Start HawkTracerClient

--- tests/bindings/nodejs/HawkTracerClient.test.ts
+++ tests/bindings/nodejs/HawkTracerClient.test.ts
@@ -1,19 +1,38 @@
-test("Create HawkTracerClient instance", () => {
-    const {HawkTracerClient} = require('bindings')('hawk_tracer_client');   // loads build/Release/hawktracer_client.node
-    const hawkTracerClient = new HawkTracerClient("some-file");
-    expect(hawkTracerClient).not.toBeNull();
+describe("1. Initiating HawkTracerClient", () => {
+    test("succeeds when constructor is given `source` parameter in string type", () => {
+        const {HawkTracerClient} = require('bindings')('hawk_tracer_client');   // loads build/Release/hawk_tracer_client.node
+        const hawkTracerClient = new HawkTracerClient("some-file");
+        expect(hawkTracerClient).not.toBeNull();
+    });
+
+    test("fails without parameter", () => {
+        const {HawkTracerClient} = require('bindings')('hawk_tracer_client');
+        expect(() => {
+            new HawkTracerClient();
+        }).toThrow();
+    });
+
+    test("fails with parameter in wrong type", () => {
+        const {HawkTracerClient} = require('bindings')('hawk_tracer_client');
+        expect(() => {
+            new HawkTracerClient(11);
+        }).toThrow();
+    });
 });
 
-test("Create HawkTracerClient instance without source", () => {
+describe("2. Start HawkTracerClient", () => {
     const {HawkTracerClient} = require('bindings')('hawk_tracer_client');
-    expect(() => {
-        new HawkTracerClient();
-    }).toThrow();
-});
 
-test("Create HawkTracerClient instance with invalid source", () => {
-    const {HawkTracerClient} = require('bindings')('hawk_tracer_client');
-    expect(() => {
-        new HawkTracerClient(11);
-    }).toThrow();
+    test("succeeds with existing source file", () => {
+        const source = require('path').join(__dirname, 'test.htdump');
+        const hawkTracerClient = new HawkTracerClient(source);
+        expect(hawkTracerClient.start()).toBe(true);
+    });
+
+    test.todo("succeeds with source in 'x.x.x.x:p' format where x.x.x.x is the IP address, p is port number");
+
+    test("succeeds with non-existing source file", () => {
+        const hawkTracerClient = new HawkTracerClient('non-existing file !@£$%^&*()');
+        expect(hawkTracerClient.start()).toBe(false);
+    });
 });
