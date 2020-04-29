1bb6d878721583161e8bf77cb8f6290c844ef4a7
Hojin Ghim <hojin@amazon.com>
2020-04-29 15:09:28 +0100

    Add TypeScript wrapper

--- examples/nodejs-client/index.js
+++ examples/nodejs-client/index.js
@@ -2,7 +2,12 @@ const {HawkTracerClient} = require('@hawktracer/client')
 
 const hawkTracerClient = new HawkTracerClient(require('path').join(__dirname, 'test.htdump'));
 // const hawkTracerClient = new HawkTracerClient('127.0.0.1:5443');
-// const hawkTracerClient = new HawkTracerClient('127.0.0.1:5443', require('path').join(__dirname, 'nativetracepoints.map'));
+/*
+const hawkTracerClient = new HawkTracerClient({
+    source: '127.0.0.1:5443',
+    map_files: require('path').join(__dirname, 'nativetracepoints.map')
+});
+*/
 
 hawkTracerClient.onEvents((events) => events.forEach(e => console.log(JSON.stringify(e))));
 
