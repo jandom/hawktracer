d6f2c361054f4622d28bead00964046a73d94bea
Jan Domanski <jdomans@amazon.com>
2020-11-09 13:41:22 +0000

    Fixing a broken build on travis

--- client_utils/include/hawktracer/client_utils/stream_factory.hpp
+++ client_utils/include/hawktracer/client_utils/stream_factory.hpp
@@ -10,7 +10,7 @@ namespace HawkTracer
 namespace ClientUtils
 {
 
-std::unique_ptr<parser::Stream> make_stream_from_string(const std::string& source_description, const bool wait_for_server);
+std::unique_ptr<parser::Stream> make_stream_from_string(const std::string& source_description, const bool wait_for_server = true);
 
 } // namespace ClientUtils
 } // namespace HawkTracer
